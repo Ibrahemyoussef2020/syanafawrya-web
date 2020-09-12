617a3ba9721bcc02c7c902b6f916310b609f1949
Ibrahemyoussef2020 <ibrahem.youssef2020@hotmail.com>
2020-09-12 11:05:57 +0300

    page-main-sharp

---
+++ sharp.js
@@ -0,0 +1,15 @@
+	
+
+  function asideScale() {
+
+	 document.getElementById("fixed-bar").style.transform ="translate(0%,0)scale(1.1,1.1)";
+	
+}
+
+
+function asideTranslate() {
+
+	 document.getElementById("fixed-bar").style.transform ="translate(-50%,0)scale(0,0)"
+	
+}
+
