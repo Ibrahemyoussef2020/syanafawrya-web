d4729c3cfdf43daf3f4dfb5dc4d35bccb2966519
Ibrahemyoussef2020 <ibrahem.youssef2020@hotmail.com>
2020-10-17 03:39:42 +0200

    ref

---
+++ j-c-m/main.js
@@ -0,0 +1,21 @@
+function asideScale() {
+
+    document.getElementById("fixed-bar").style.transform ="translate(0%,0)scale(1.1,1.1)";
+   
+}
+
+
+function asideTranslate() {
+
+    document.getElementById("fixed-bar").style.transform ="translate(-50%,0)scale(0,0)"
+   
+}
+
+
+document.getElementById("prompt").onclick = function(){
+
+
+
+    var a = prompt("للاطلاع على المزيد أدخل إيميلك")
+
+}
