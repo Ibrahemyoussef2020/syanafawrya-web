e1e482fac027375ebc7d5734002dd05a08ed4197
unknown <ibrahem.youssef2020@hotmail.com>
2022-04-02 21:35:19 +0200

    frist mobdified

--- j-c-m/main.js
+++ j-c-m/main.js
@@ -1,21 +1,19 @@
 function asideScale() {
-
-    document.getElementById("fixed-bar").style.transform ="translate(0%,0)scale(1.1,1.1)";
-   
+    document.getElementById("fixed-bar").style.transform ="translate(0%,0)scale(1.1,1.1)";  
 }
-
 
 function asideTranslate() {
-
-    document.getElementById("fixed-bar").style.transform ="translate(-50%,0)scale(0,0)"
-   
+    document.getElementById("fixed-bar").style.transform ="translate(-50%,0)scale(0,0)"  
 }
+/*****machine***** */
+const  articleI = document.querySelectorAll("article > div");
+const  articleB = document.querySelectorAll("article > p");
 
-
-document.getElementById("prompt").onclick = function(){
-
-
-
-    var a = prompt("للاطلاع على المزيد أدخل إيميلك")
-
+for(let s = 0 ; s < articleI.length ; s++){
+    articleI[s].addEventListener("click" , _=>{
+        articleB.forEach(e => {
+            e.title == articleI[s].className ? e.classList.toggle("fit-content") : "";   
+        });
+    })
+    console.log(articleI[s])
 }
